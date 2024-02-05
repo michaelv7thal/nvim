5612d730185c307f08058f469b76f0d1054d1f33
Michael von Siebenthal <michael.vonsiebenthal@urban-connect.ch>
2024-02-05 11:45:37 +0100

    added dbtpal

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,7 +2,7 @@
 
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "sql", "rust", "maintained" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "sql", "rust" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
