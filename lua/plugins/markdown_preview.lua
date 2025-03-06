b09f9d42ea8129f17bf0fd3ccd130b55f93f5bc1
Michael von Siebenthal <michael.vonsiebenthal@implenia.com>
2025-03-06 08:55:31 +0100

    updated mardkown previe

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -1,9 +1,14 @@
 return {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && yarn install",  -- required build step for the plugin
+    ft = "markdown",                    -- lazy-load on markdown filetypes
+    init = function()
+      -- Set options before the plugin loads
+      vim.g.mkdp_auto_start = 1      -- auto start preview when opening markdown
+      vim.g.mkdp_auto_close = 1      -- auto close preview when leaving markdown
+      vim.g.mkdp_host = "172.31.112.1"    -- bind to localhost to restrict access
+      vim.g.mkdp_port = "8080"         -- you can change this if needed
+    end,
+  },
 }
